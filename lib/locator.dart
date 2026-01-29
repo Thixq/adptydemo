a6e440c7904cc78ee0283676c9c8a44966c3709e
Kaan <kaefean1@gmail.com>
2026-01-29 15:04:42 +0300

    Feat: Implement Dependency Injection with GetIt and setup locator

---
+++ lib/locator.dart
@@ -0,0 +1,20 @@
+import 'package:adapty_flutter/adapty_flutter.dart';
+import 'package:adptydemo/service_and_managers/adapty_service.dart';
+import 'package:adptydemo/service_and_managers/note_db_service.dart';
+import 'package:adptydemo/service_and_managers/note_manager.dart';
+import 'package:adptydemo/service_and_managers/user_profile_manager.dart';
+import 'package:get_it/get_it.dart';
+
+/// Service locator instance.
+final GetIt locator = GetIt.instance;
+
+/// Sets up the service locator with necessary services.
+void setupLocator() {
+  locator
+    ..registerLazySingleton<AdaptyService>(
+      () => AdaptyService(adapty: Adapty()),
+    )
+    ..registerLazySingleton<NoteDBService>(NoteDBService.new)
+    ..registerLazySingleton<UserProfileManager>(UserProfileManager.new)
+    ..registerLazySingleton<NoteManager>(NoteManager.new);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
-import 'package:adapty_flutter/adapty_flutter.dart';
 import 'package:adptydemo/env/dev_env.dart';
+import 'package:adptydemo/locator.dart';
 import 'package:adptydemo/page/root_page.dart';
-import 'package:adptydemo/service/adapty_service.dart';
-import 'package:adptydemo/service/note_service.dart';
+import 'package:adptydemo/service_and_managers/adapty_service.dart';
+import 'package:adptydemo/service_and_managers/note_db_service.dart';
 import 'package:flutter/cupertino.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await NoteService().init();
-  await AdaptyService(adapty: Adapty()).initialize(
+  setupLocator();
+  await locator<NoteDBService>().init();
+  await locator<AdaptyService>().initialize(
     apiKey: DevEnv().adaptyApiKey,
   );
   runApp(const MyApp());
@@ -18,7 +19,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   /// Creates a [MyApp] instance.
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const CupertinoApp(
