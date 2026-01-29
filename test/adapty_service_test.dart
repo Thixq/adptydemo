3970cf61f85cdd29a5bc422087617590c367633f
Kaan <kaefean1@gmail.com>
2026-01-29 15:05:45 +0300

    fix

--- test/adapty_service_test.dart
+++ test/adapty_service_test.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:adapty_flutter/adapty_flutter.dart';
-import 'package:adptydemo/service/adapty_service.dart';
+import 'package:adptydemo/service_and_managers/adapty_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
